105883af44bd8b0219f729fb9f8d825f1d591959
Andy Shinn <andys@andyshinn.as>
2026-04-08 17:48:51 -0500

    custom radio stuff

--- variants/ikoka_handheld_nrf/variant.h
+++ variants/ikoka_handheld_nrf/variant.h
@@ -35,7 +35,8 @@ extern "C"
 #define LED_GREEN               (13)
 #define LED_BLUE                (12)
 
-#define LED_STATE_ON            (1)     // State when LED is litted
+#define PIN_STATUS_LED          LED_GREEN
+#define LED_STATE_ON            (0)     // State when LED is litted
 
 // Buttons
 #define PIN_BUTTON1             (PINS_COUNT)
